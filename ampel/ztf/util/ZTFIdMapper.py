c213a39032f8af013daa58d94a544626ae5b0335
simeonreusch <simeon.reusch@desy.de>
2023-02-17 16:28:26 +0100

    added ZTFNoisifiedIdMapper to deal with ZTF-IDs appended by an integer

--- ampel/ztf/util/ZTFIdMapper.py
+++ ampel/ztf/util/ZTFIdMapper.py
@@ -147,6 +147,45 @@ class ZTFIdMapper(AbsIdMapper):
 		else:
 			raise TypeError(f"Ampel ids for ZTF transients should be ints (got {type(ampel_id)} {ampel_id})")
 
+class ZTFNoisifiedIdMapper(AbsIdMapper):
+    def __init__(self):
+        ZTFIdMapper.__init__(self)
+
+    @classmethod
+    def to_ampel_id(cls, ztf_id: str) -> int:
+        """
+        Append an integer to a padded Ampel ID.
+        This is useful for e.g. noisified versions of the
+        same parent lightcurve
+        """
+        split_str = ztf_id.split("_")
+        ampel_id_part = split_str[0]
+
+        ampel_id = ZTFIdMapper.to_ampel_id(ztf_id)
+
+        if len(split_str) > 1:
+            sub_id = split_str[1]
+            return int(str(ampel_id) + "00000000" + sub_id)
+
+        else:
+            return ampel_id
+
+    @classmethod
+    def to_ext_id(cls, ampel_id_with_sub_id: StockId) -> str:
+        """
+        Return the original name of the noisified lightcurve
+        """
+        both_ids = str(ampel_id_with_sub_id).split("00000000")
+        ampel_id = int(both_ids[0])
+
+        ztfid = ZTFIdMapper.to_ext_id(ampel_id)
+
+        if len(both_ids) > 1:
+            sub_id = int(both_ids[1])
+            return ztfid + "_" + str(sub_id)
+
+        else:
+            return ztfid
 
 # backward compatibility shortcuts
 to_ampel_id = ZTFIdMapper.to_ampel_id
