b77c41d1909746c197bab91952b723e0761bdb0d
Jakob van Santen <jvansanten@gmail.com>
2023-09-11 14:29:46 +0200

    AllConsumingConsumer: ignore offsets from unknown partitions
    Fixes up ea0b4e038025bf5b3554331478c7d7a3d6770ff4

--- ampel/ztf/t0/load/AllConsumingConsumer.py
+++ ampel/ztf/t0/load/AllConsumingConsumer.py
@@ -165,7 +165,19 @@ class AllConsumingConsumer:
     ):
         if self._logger:
             self._logger.debug(f"Storing offsets: {offsets}")
-        self._consumer.store_offsets(offsets=offsets)
+        try:
+            self._consumer.store_offsets(offsets=offsets)
+        except confluent_kafka.KafkaException as exc:
+            # librdkafka will refuse to store offsets on a partition that is not
+            # currently assigned. this can happen if the group is rebalanced
+            # while a batch of messages is in flight. see also:
+            # https://github.com/confluentinc/confluent-kafka-dotnet/issues/1861
+            err = exc.args[0]
+            if err.code() == confluent_kafka.KafkaError._STATE:
+                ...
+            else:
+                raise KafkaError(err)
+
 
     def commit(self):
         if self._offsets:
