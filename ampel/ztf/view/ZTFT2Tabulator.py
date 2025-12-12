2bfe779454569c17e3eeaed1bcd7d8438b6a303a
Jakob van Santen <jvansanten@gmail.com>
2025-12-12 09:35:54 +0100

    Move superseded datapoint detection from ZiMongoMuxer to ZTFT2Tabulator
    Muxers no longer have access to DBUpdatesBuffer, and this was never a great design in the first place.

--- ampel/ztf/view/ZTFT2Tabulator.py
+++ ampel/ztf/view/ZTFT2Tabulator.py
@@ -6,6 +6,7 @@
 # Last Modified Date: 05.05.2022
 # Last Modified By  : Marcus Fenner <mf@physik.hu-berlin.de>
 
+from bisect import bisect_right
 from collections.abc import Iterable, Sequence
 from typing import Any
 
@@ -14,7 +15,7 @@ from astropy.table import Table
 
 from ampel.abstract.AbsT2Tabulator import AbsT2Tabulator
 from ampel.content.DataPoint import DataPoint
-from ampel.types import StockId
+from ampel.types import DataPointId, StockId
 from ampel.util.collections import ampel_iter
 from ampel.ztf.util.ZTFIdMapper import ZTFIdMapper
 
@@ -38,8 +39,31 @@ class ZTFT2Tabulator(AbsT2Tabulator):
     )  # Immediately reject flux outliers beyond. 0 means none
     flux_max: float = 10**30  # Cut flux above this 0 means none
 
+    # Check for reprocessed datapoints
+    check_reprocessing: bool = True
+
     def filter_detections(self, dps: Iterable[DataPoint]) -> Iterable[DataPoint]:
-        return [dp for dp in dps if "ZTF" in dp["tag"] and "magpsf" in dp["body"]]
+        dp_ids = {dp["id"]: dp for dp in dps if "tag" in dp and "ZTF" in dp["tag"]}
+        if self.check_reprocessing:
+            # uniquify photopoints by jd, rcid. For duplicate points, choose the
+            # one with the larger id (jd, rcid) -> ids
+            unique_dps_ids: dict[tuple[float, int], list[DataPointId]] = {}
+            for dp in dp_ids.values():
+                # jd alone is actually enough for matching pps reproc, but an upper limit can
+                # be associated with multiple stocks at the same jd. here, match also by rcid
+                key = (dp["body"]["jd"], dp["body"]["rcid"])
+
+                if target := unique_dps_ids.get(key):
+                    # insert id in order
+                    idx = bisect_right(target, dp["id"])
+                    if idx == 0 or target[idx - 1] != dp["id"]:
+                        target.insert(idx, dp["id"])
+                else:
+                    unique_dps_ids[key] = [dp["id"]]
+            final_dps_set = {v[-1] for v in unique_dps_ids.values()}
+        else:
+            final_dps_set = set(dp_ids.keys())
+        return [dp_ids[dp_id] for dp_id in final_dps_set]
 
     def get_flux_table(
         self,
@@ -99,9 +123,8 @@ class ZTFT2Tabulator(AbsT2Tabulator):
     def get_stock_name(self, dps: Iterable[DataPoint]) -> list[str]:
         return [ZTFIdMapper.to_ext_id(el) for el in self.get_stock_id(dps)]
 
-    @staticmethod
     def get_values(
-        dps: Iterable[DataPoint], params: Sequence[str]
+        self, dps: Iterable[DataPoint], params: Sequence[str]
     ) -> tuple[Sequence[Any], ...]:
         if tup := tuple(
             map(
@@ -109,8 +132,7 @@ class ZTFT2Tabulator(AbsT2Tabulator):
                 zip(
                     *(
                         [el["body"][param] for param in params]
-                        for el in dps
-                        if "ZTF" in el["tag"]
+                        for el in self.filter_detections(dps)
                     ),
                     strict=False,
                 ),
